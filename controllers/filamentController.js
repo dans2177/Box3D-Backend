f752967488b680340990a23d5fe81f262808b1c1
Daniel <daniel@thebox3d.com>
2023-12-12 08:58:54 -0700

    0.0.6

--- controllers/filamentController.js
+++ controllers/filamentController.js
@@ -52,26 +52,55 @@ exports.postFilament = async (req, res) => {
   }
 };
 
-//----------------------------------------------------------------
-// SUBTRACTION CONTROLLERS
-
-//Create Subtraction
-exports.createSubtraction = async (req, res) => {
+// Update Filament
+exports.updateFilament = async (req, res) => {
   try {
-    const { filamentId, subtractionLength } = req.body;
-    const filament = await Filament.findById(filamentId);
+    const filamentId = req.params.filamentId;
+    const updateData = req.body;
 
-    // Check if filament exists
+    // Find the filament and update it
+    const filament = await Filament.findByIdAndUpdate(filamentId, updateData, { new: true });
+
     if (!filament) {
       return res.status(404).json({ message: "Filament not found" });
     }
 
-    // Add the new subtraction
-    filament.subtractions.push({ subtractionLength });
+    res.json({ message: "Filament updated successfully", data: filament });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-    filament.recalculateCurrentAmount();
+// Delete Filament
+exports.deleteFilament = async (req, res) => {
+  try {
+    const filamentId = req.params.filamentId;
 
-    await filament.save();
+    // Delete the filament
+    const filament = await Filament.findByIdAndDelete(filamentId);
+
+    if (!filament) {
+      return res.status(404).json({ message: "Filament not found" });
+    }
+
+    res.json({ message: "Filament deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Get Single Filament
+exports.getFilament = async (req, res) => {
+  try {
+    const filamentId = req.params.filamentId;
+    const filament = await Filament.findById(filamentId);
+
+    if (!filament) {
+      return res.status(404).json({ message: "Filament not found" });
+    }
+
     res.json({ data: filament });
   } catch (error) {
     console.error(error);
@@ -79,28 +108,92 @@ exports.createSubtraction = async (req, res) => {
   }
 };
 
-//Delete Subtraction
-exports.deleteSubtraction = async (req, res) => {
+
+//----------------------------------------------------------------
+// SUBTRACTION CONTROLLERS
+
+// Create Subtraction
+exports.createSubtraction = async (req, res) => {
   try {
-    const { filamentId, subtractionId } = req.body;
+    const filamentId = req.params.filamentId;
+    const { subtractionLength, project, date } = req.body;
+
+    // Find the filament and add the new subtraction
     const filament = await Filament.findById(filamentId);
-
-    // Check if filament exists
-    if (!filament) {
-      return res.status(404).json({ message: "Filament not found" });
-    }
-
-    // Remove the subtraction
-    filament.subtractions.id(subtractionId).remove();
-
-    filament.recalculateCurrentAmount();
-
+    filament.subtractions.push({ subtractionLength, project, date });
+    filament.recalculateCurrentAmount(); // Recalculate the current amount
     await filament.save();
-    res.json({ data: filament }).status(200);
+
+    res.status(201).json({ message: "Subtraction added successfully", data: filament });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+// Get Subtractions
+exports.getSubtractions = async (req, res) => {
+  try {
+    const filamentId = req.params.filamentId;
+    const filament = await Filament.findById(filamentId);
+
+    if (!filament) {
+      return res.status(404).json({ message: "Filament not found" });
+    }
+
+    res.json({ data: filament.subtractions });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Update Subtraction
+exports.updateSubtraction = async (req, res) => {
+  try {
+    const filamentId = req.params.filamentId;
+    const subtractionId = req.params.subtractionId;
+    const updateData = req.body;
+
+    // Find the filament and update the specific subtraction
+    const filament = await Filament.findById(filamentId);
+    const subtraction = filament.subtractions.id(subtractionId);
+    if (subtraction) {
+      Object.assign(subtraction, updateData);
+      filament.recalculateCurrentAmount(); // Recalculate the current amount
+      await filament.save();
+      res.json({ message: "Subtraction updated successfully", data: subtraction });
+    } else {
+      res.status(404).json({ message: "Subtraction not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Delete Subtraction
+exports.deleteSubtraction = async (req, res) => {
+  try {
+    const filamentId = req.params.filamentId;
+    const subtractionId = req.params.subtractionId;
+
+    // Find the filament and remove the specific subtraction
+    const filament = await Filament.findById(filamentId);
+    const subtraction = filament.subtractions.id(subtractionId);
+    if (subtraction) {
+      subtraction.remove();
+      filament.recalculateCurrentAmount(); // Recalculate the current amount
+      await filament.save();
+      res.json({ message: "Subtraction deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Subtraction not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 
 //----------------------------------------------------------------
--- routes/filamentRoutes.js
+++ routes/filamentRoutes.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 const filamentController = require("../controllers/filamentController");
 
-// Filament Router
+// Routes for Filament
 router.get("/", filamentController.getFilaments);
+router.get("/:filamentId", filamentController.getFilament);
 router.post("/", filamentController.postFilament);
-router.post("/subtract", filamentController.createSubtraction);
-router.post("/remSubtract", filamentController.deleteSubtraction);
+router.put("/:filamentId", filamentController.updateFilament);
+router.delete("/:filamentId", filamentController.deleteFilament);
 
+// Routes for Subtractions
+router.post("/:filamentId/subtraction", filamentController.createSubtraction); // Adds a subtraction to a filament
+router.put( "/:filamentId/subtraction/:subtractionId", filamentController.updateSubtraction);
+router.delete("/:filamentId/subtraction/:subtractionId",filamentController.deleteSubtraction);
+router.get("/:filamentId/subtraction/:subtractionId",filamentController.getSubtractions);
 
 module.exports = router;
